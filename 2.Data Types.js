1b4d9ef99241c0fa11ec998e7dc0c4d9b97162cb
haseebpoolakkal <haseebpoolakkal@gmail.com>
2022-08-02 08:41:09 +0530

    Initial Commit

---
+++ 2.Data Types.js
@@ -0,0 +1,14 @@
+var name = "John";
+var age = 18;
+var isMale = true;
+var dob = new Date(2004, 1, 26);
+var address = {};
+var empty = undefined;
+var array = ["A", "B", "C"];
+
+console.log(name);
+console.log(age);
+console.log(isMale);
+console.log(dob);
+console.log(address);
+console.log(empty);
---
+++ 4.Functions.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    console.log("Inside function");
+}
+
+myFunction();
